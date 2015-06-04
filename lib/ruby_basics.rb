0c0051587f4a9ec9e5b9c4f08ab9c91f1150b76d
deniznida <denizunat@hotmail.com>
2015-06-04 15:25:32 -0400

    change test suite to not require answering in rspec

---
+++ lib/ruby_basics.rb
@@ -0,0 +1,32 @@
+
+
+def division(num1, num2)
+  
+end
+
+def assign_variable(value)
+  
+end
+
+def argue
+  
+end
+
+
+def greeting
+  puts "#{greeting}, #{name}"
+end
+
+def return_a_value
+  
+end
+
+
+
+def last_evaluated_value
+  "beginner"
+end
+
+def pizza_party
+
+end
