65f8fd70335468e587ee4056ed0f452d454b2756
Charles Pletcher <cpletcher2@gmail.com>
2016-04-05 16:31:50 -0400

    Clarify specs
    Fixes #20

--- spec/ruby_basics_spec.rb
+++ spec/ruby_basics_spec.rb
@@ -8,13 +8,13 @@ describe "ruby" do
     it 'given two numbers returns quotient of the first number divided by the second number' do
       num1 = 42
       num2 = 7
-      
+
       expect(division(num1,num2)).to eq(6)
     end
   end
 
   describe '#assign_variable' do
-    it 'should take an argument of a persons name and assign it to a variable name' do
+    it 'takes an argument of a persons name and assign it to a variable name' do
       value = "Bob"
 
       expect(assign_variable(value)).to eq(value)
@@ -22,22 +22,22 @@ describe "ruby" do
   end
 
   describe '#argue' do
-    it 'should add an argument to the method argue' do
+    it 'accepts an argument and return that argument as is' do
       phrase = "I'm right and you are wrong!"
-      
+
       expect(argue(phrase)).to eq(phrase)
-    end  
+    end
   end
 
   describe '#greeting' do
-    it 'should take in two arguments' do
+    it 'takes two arguments' do
       greeting = "Hi there, "
       name = "Bobby!"
 
       expect{ greeting(greeting, name) }.to_not raise_error
     end
 
-    it 'should raise an error with one argument' do
+    it 'raises an error with one argument' do
       greeting = "Hi there, "
 
       expect{ greeting(greeting)}.to raise_error
@@ -45,23 +45,23 @@ describe "ruby" do
   end
 
   describe '#return_a_value' do
-    it 'return the phrase "Nice"' do
+    it 'returns the phrase "Nice"' do
       expect(return_a_value).to eq("Nice")
     end
   end
 
   describe '#last_evaluated_value' do
-    it 'return the phrase "expert"' do
+    it 'returns the phrase "expert"' do
       expect(last_evaluated_value).to eq("expert")
     end
   end
 
   describe '#pizza_party' do
-    it 'has an optional argument that defaults to "cheese" and returns "cheese" when called without an argument' do
+    it 'returns "cheese" by default' do
       expect(pizza_party).to eq("cheese")
     end
 
-    it 'can be called with an argument and return the string that was passed in as an argument' do
+    it 'returns the argument it received' do
       expect(pizza_party("pepperoni")).to eq("pepperoni")
     end
   end
