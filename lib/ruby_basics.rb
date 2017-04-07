38158ebc7262b2f2a34e4e8c0fd49e8767a1167c
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-04-06 21:16:07 -0500

    the bepis that refrepis

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  result = num1 / num2
+  result
 end
 
 def assign_variable(value)
-  
+  variable = value
+  variable
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
+  phrase
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(arg = "cheese")
+  arg
 end
