f5f09ebbe3d080ee8d022dc352bdf8a7aaa7630b
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-04-13 19:07:32 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(arg1, arg2)
 end
 
 def return_a_value
-  return "Nice"
+  "Nice"
 end
 
 def last_evaluated_value
-  return "expert"
+  "expert"
 end
 
 def pizza_party(value = "cheese")
-  return value
+  value
 end
