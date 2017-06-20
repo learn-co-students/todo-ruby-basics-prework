a23ab062142b2c319490ad84c99bdcdf008e53cd
Roman Mulladzhanov <romanmullady@gmail.com>
2017-06-20 23:32:15 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  bird = value
+
 end
 
-def argue
-  
+def argue(value)
+  value
+
 end
 
-def greeting
+def greeting(value1,value2)
+
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(bird = "cheese")
+  bird
 end
