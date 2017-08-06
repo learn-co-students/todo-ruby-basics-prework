9e8b08b4056ea74eb0fb0a203e7394c874090c56
Todd Greenberg <tsgreenberg1217@gmail.com>
2017-08-06 16:19:30 +0000

    filled out functions

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
- 
+  return num1/num2
+
 end
 
 def assign_variable(value)
-  
+  var = value
+
 end
 
-def argue
-  
+def argue(input)
+  return input
+
 end
 
-def greeting
+def greeting(greeting,name)
+
 
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party(input = "cheese")
+  return input
+
 
 end
