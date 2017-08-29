000f03de5f7db12c1958357aaa5edb2711040a9a
Justin Frawley <freegamespeech@gmail.com>
2017-08-29 21:40:17 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,15 +3,15 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-"#{value}"
+name = value
 end
 
 def argue(argument)
-return argument
+argument
 end
 
 def greeting(greeting, name)
-
+puts "#{greeting}, #{name}"
 end
 
 def return_a_value
