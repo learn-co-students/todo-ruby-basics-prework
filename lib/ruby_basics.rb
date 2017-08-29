b373a469d42fec7ea0257968263805008eddc553
github username <github email address>
2017-08-29 18:42:54 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+	num1 / num2
 end
 
 def assign_variable(value)
-  
+	var = value
+	var
 end
 
-def argue
-  
+def argue(value)
+	value
 end
 
-def greeting
-
+def greeting(greeting, name)
+	
 end
 
 def return_a_value
-
+	"Nice"
 end
 
 def last_evaluated_value
-
+	"expert"
 end
 
-def pizza_party
-
+def pizza_party(topping =  "cheese")
+	topping
 end
