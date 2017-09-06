b2ee55da7a7bd58aa25e04723d808d690c0c2e7f
Alona Yeheskel <alona.yeheskel@gmail.com>
2017-09-06 16:43:21 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
+  return name
 end
 
-def argue
-  
+def argue(item)
+ return item
 end
 
-def greeting
+def greeting(string, name)
 
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
-
+def pizza_party(argument = "cheese")
+  return argument
 end
