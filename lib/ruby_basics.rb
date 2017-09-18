d11edd7fe61ddb31e28f8ca8bc61fbaef6e1fb1e
Winter LaMon <winter.lamon@gmail.com>
2017-09-18 05:02:46 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  value = "#{value}"
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase
 end
 
-def greeting
-
+def greeting(salutation, name)
+  puts "#{salutation} #{name}!"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza_type = "cheese")
+  return pizza_type
 end
