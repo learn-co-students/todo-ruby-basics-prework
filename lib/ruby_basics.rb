e33e5ce2aa90cde907519ed58193d29e65f3a544
Elisa Vaccaro <erv230@nyu.edu>
2017-09-22 17:45:31 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  variable = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(arg1, arg2)
+  arg1 + arg2
 end
 
 def return_a_value
-
+  value = "Nice"
 end
 
 def last_evaluated_value
-
+  last = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
