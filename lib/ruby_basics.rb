3a93f8ef40b3d20c13be7f2277644b1321785129
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-19 17:31:20 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue(arg)
+return arg
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
-
+def return_a_value(phrase = "Nice")
+  return phrase
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(toppings = "cheese")
+  return toppings
 end
