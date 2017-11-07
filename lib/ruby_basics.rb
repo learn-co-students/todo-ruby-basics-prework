2d594b7fa1325869a4094eba4063f5eac3ef9afd
Garry <clergeg@gmail.com>
2017-11-07 00:35:25 -0500

    Lab Commit

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ quotient = num1 / num2
+ quotient
 end
 
 def assign_variable(value)
-  
+  name = value
+  value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  type
 end
