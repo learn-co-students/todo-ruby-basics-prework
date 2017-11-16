32a0e9c005420fc451ff3e03d75bf04066f2683f
TorreJohnson <torre.johnson@gmail.com>
2017-11-16 00:49:20 -0500

    passing all tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  answer = num1 / num2
+  answer
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting(greeting, name)
 
 end
 
-def last_evaluated_value
-
+def return_a_value(phrase = "Nice")
+  phrase
 end
 
-def pizza_party
+def last_evaluated_value(level = "expert")
+  level
+end
 
+def pizza_party(topping = "cheese")
+  topping
 end
