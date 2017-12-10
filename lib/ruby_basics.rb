475d57ffeaf85a961480d5f161d31220303280d7
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2017-12-10 18:50:06 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,22 +6,24 @@ def assign_variable(value)
   name = value
 end
 
-def argue
-
+def argue(arguement)
+  arguement
 end
 
-def greeting
+def greeting(phrase, name)
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
+  phrase
 end
 
 def last_evaluated_value
-
+  value = "expert"
+  value
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
