c351abaf249dba03bae9d40e25e81c20aa4e28f0
Stacey  Marks <staceyamarks@gmail.com>
2018-01-18 15:09:59 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value 
+  name
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
+  "ok"
+  "expert"
 
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  type
 end
