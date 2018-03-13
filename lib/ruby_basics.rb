a01ecd4d084a95efce19b151867376d0fc772b63
briecodes <hello@brie.work>
2018-03-12 20:44:30 -0400

    Methods completed.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(ment)
+  ment
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting
+  name
 end
 
-def return_a_value
-
+def return_a_value(value="Nice")
+  value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value="expert")
+  value
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
