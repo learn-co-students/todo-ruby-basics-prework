0add5a9758f30d433054d5b1fe1a431adadfb535
Mary-Kate James <marykatj@gmail.com>
2018-04-20 15:57:45 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  elephant = value
 end
 
-def argue
-  
+def argue(pizza)
+  return "#{pizza}"
 end
 
-def greeting
-
+def greeting(arg1, arg2)
+  return "Hello #{arg1}, king of #{arg2}!"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  return "#{topping}"
 end
