9ceaaf2f82dab08606a2e487c19c4a66b77e2cc8
critsmet <36425627+critsmet@users.noreply.github.com>
2018-05-05 17:17:24 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+return num1/num2
 end
 
 def assign_variable(value)
-  
+name = value
+return name
 end
 
-def argue
-  
+def argue(ment)
+return ment
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+  return "Nice"
 
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+  return topping
 
 end
