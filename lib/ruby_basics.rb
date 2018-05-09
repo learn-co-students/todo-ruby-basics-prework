3096b70dd4811aeb687a019911f44d7b5b448bb4
hyprul <hyprul@ucla.edu>
2018-05-08 21:46:56 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ 	num1/num2
 end
 
 def assign_variable(value)
+	name = value
+end
+
+def argue(input)
+	input
   
 end
 
-def argue
-  
-end
+def greeting(greeting, name)
 
-def greeting
 
 end
 
 def return_a_value
-
+	return "Nice"
 end
 
 def last_evaluated_value
-
+	return "expert"
 end
 
-def pizza_party
-
+def pizza_party(guests = "cheese")
+	guests
 end
