05843374f9c24f98acaf22ddae43d2482267d277
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-20 04:00:46 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -7,24 +7,23 @@ def assign_variable(value)
 end
 
 def argue (phrase = "I'm right and you are wrong!")
-  puts "#{phrase}"
-end
-argue()
-
-def greeting (greeting = "Hi, there", name)
-  puts "#{greeting}" + name 
-end
-greeting("Bobby")
-
-
-def return_a_value
-
+   phrase
 end
 
-def last_evaluated_value
-
+def greeting (arg1,arg2)
+  
 end
 
-def pizza_party
-
+def return_a_value(phrase = "Nice")
+  phrase
 end
+
+def last_evaluated_value 
+  puts "expert"
+  "expert"
+end
+
+def pizza_party(phrase = "cheese")
+  phrase
+end
+pizza_party("pepperoni")
