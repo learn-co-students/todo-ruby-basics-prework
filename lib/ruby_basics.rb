08a491ee15284a98444329daf126d02840fecbe5
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-20 03:40:31 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,18 +1,21 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue (phrase = "I'm right and you are wrong!")
+  puts "#{phrase}"
 end
+argue()
 
-def greeting
-
+def greeting (greeting = "Hi, there", name)
+  puts "#{greeting}" + name 
 end
+greeting("Bobby")
+
 
 def return_a_value
 
