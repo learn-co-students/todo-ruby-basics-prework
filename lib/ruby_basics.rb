cc6d6ad6ed6bdb0ea6fe2b56a9d327e60b81a153
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-28 10:48:03 +0000

    basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(word)
+  word
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
+  phrase
 end
 
 def last_evaluated_value
-
+  value = "expert"
+  value
 end
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+  pizza
 end
