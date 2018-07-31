68fbd5dd0c4260a1a584bbb41986af21fa70175d
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-31 04:17:05 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  jam = value
 end
 
-def argue
-  
+def argue(jj)
+  jj
 end
 
-def greeting
+def greeting(kocho,nibo)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(kol = "cheese")
+  kol
 end
