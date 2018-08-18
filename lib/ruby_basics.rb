c51c514387270c73b7fd2e89fbc6a5b9bd3d2a32
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-18 19:50:51 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ 12/2
 end
 
 def assign_variable(value)
-  
+  name = "Bob"
 end
 
-def argue
-  
+def argue(butwhy)
+  butwhy
 end
 
-def greeting
-
+def greeting(salutation, name)
+  "Good #{salutation} my dear friend #{name}"
 end
 
 def return_a_value
-
+"Nice"
 end
 
 def last_evaluated_value
-
+"expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
