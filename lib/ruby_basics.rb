5ed5979c9af2912e79d541abc7e44acb41494e5b
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-20 06:17:01 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts greeting + " " + name
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "Nice"
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+  pizza
 end
