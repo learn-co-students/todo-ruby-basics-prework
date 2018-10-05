39a00838f23c97d307b2f770a7a6964ef6ccd8f6
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-05 03:39:30 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  message=value
+  message
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
-
+def greeting(greeting,name)
+puts #{greeting}, #{name}
 end
 
 def return_a_value
-
+value="Nice"
+value
 end
 
 def last_evaluated_value
-
+value="expert"
 end
 
-def pizza_party
-
+def pizza_party(fooditem="cheese")
+  fooditem
 end
