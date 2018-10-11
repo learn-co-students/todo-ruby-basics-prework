9945ac901d41f5a029490aaa3f766a406ee9f6e8
heather sterman <heather.sterman531@gmail.com>
2018-10-11 01:58:23 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ 42 / 7
 end
 
 def assign_variable(value)
-  
+  value = ""
 end
 
-def argue
-  
+def argue("No")
+  return "#{argue}"
 end
 
-def greeting
+def greeting("Hi") 
+  puts "Hola"
+  return "#{greeting}"
+end
+
+def return_a_value("nice")
+return "#{return_a_value}"
+end
+
+def last_evaluated_value("expert")
+  return "#{last_evaluated_value}"
 
 end
 
-def return_a_value
-
-end
-
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party("cheese")
+  puts "mushroom"
+  return "#{pizza_party}"
 end
