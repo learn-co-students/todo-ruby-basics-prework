5ca2c56f5ad4d656ce78daab31bde2fa9fef90cc
joneseyit <joneseyit@protonmail.com>
2018-10-13 18:36:18 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
+  curiousVar = value
+end
+
+def argue(arg)
+  arg
   
 end
 
-def argue
-  
-end
-
-def greeting
-
+def greeting(greeting, name)
+  "#{greeting}, #{name}!"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(kind = "cheese")
+  kind
 end
