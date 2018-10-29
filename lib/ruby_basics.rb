7816bdc59001acc70e1e0bee802dcbfca0f14115
Matthew Doering <mattdoering86@gmail.com>
2018-10-29 14:43:49 -0400

    updated ruby basics with answers

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name ="Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(name, string)
+  string = "Hello"
+  name = "Matt"
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
