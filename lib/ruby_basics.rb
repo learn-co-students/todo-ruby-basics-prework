410cc2764ae7d5ebb77784f77009f22fc8070b2a
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-05 15:23:09 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1 = 12
+ num2 = 2
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(arg1)
+  arg1
 end
 
-def greeting
+def greeting(grereting, name)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+"expert"
 end
 
-def pizza_party
-
+def pizza_party(arg2="cheese")
+arg2
 end
