526a8d0968812a37f73c46b617df38069d32caa0
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-04 13:45:41 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
+
+division(4,2)
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+assign_variable("Bob")
+
+def argue(name)
+  return "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  return type
 end
