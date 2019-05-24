7f873b737b0f90864846fc3840626355863f0dc4
Resham Arora <resham.arora@gmail.com>
2019-05-24 20:57:41 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+ num1 = 36
+ num2 = 6
+ return num = num1/num2
 end
 
 def assign_variable(value)
-  
+  return value 
 end
 
-def argue
-  
+def argue(value)
+  return value
 end
 
-def greeting
-
+def greeting(greeting, name)
+  return greeting
+  return name
 end
 
 def return_a_value
-
+ phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  puts topping
+  phrase = topping
 end
