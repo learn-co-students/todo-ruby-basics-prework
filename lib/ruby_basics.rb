314a09c1f1fccaa1412b37adff98e9c770004e7a
Dylan Harris <dylanharris23@gmail.com>
2019-05-26 00:20:39 +0000

    WIP

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+return "#{value}"
 end
 
-def argue
-  
+def argue(phrase)
+return "#{phrase}"
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting(gretting, name)
 
 end
 
-def last_evaluated_value
+def return_a_value(phrase = "Nice")
+  return "#{"Nice"}"
 
 end
 
-def pizza_party
+def last_evaluated_value(phrase = "expert")
+  return "#{phrase}"
 
+end
+
+def pizza_party(cheese = "pepperoni")
+return "#{"cheese"}"
+return "#{"pepperoni"}"
 end
